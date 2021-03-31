0c0dd194c80ab41940e6d2eae152966485e78321
miiunii <dnflwlq3231@naver.com>
2021-03-31 15:50:10 +0900

    type: ADD body: parallax scroll 2  완료

--- parallaxScrolling/index.js
+++ parallaxScrolling/index.js
@@ -1,10 +1,7 @@
-scroll = window.pageYOffset
-
 document.addEventListener('scroll', () => {
-  var offset = window.pageYOffset;
-  scroll = offset
-  document.getElementById('layer1').style.width = (100 + scroll/5) + '%'
-  document.getElementById('layer2').style.width = (100 + scroll/5) + '%'
-  document.getElementById('layer2').style.left = (scroll/50) + '%'
-  document.getElementById('text').style.top = (- scroll/5) + '%'
+  const scrollData = window.scrollY;
+  document.getElementById('layer1').style.width = (100 + scrollData/5) + '%'
+  document.getElementById('layer2').style.width = (100 + scrollData/5) + '%'
+  document.getElementById('layer2').style.left = (scrollData/50) + '%'
+  document.getElementById('text').style.top = (- scrollData/5) + '%'
 })
